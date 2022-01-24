1bcc3e611887db2e58968e2a45b69f26ee58e237
dganai <dinaganai@gmail.com>
2022-01-24 23:48:22 +0000

    adding 2nd and 3rd function to promise chain

--- index2.js
+++ index2.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
   .then(body => console.log(body));
